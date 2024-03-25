6273c83e1b289017b1c405c2df19ff878d5c35af
Ryohei Hachisuga <ryohei.8suga@gmail.com>
2024-03-25 16:53:06 +0900

    set up basic authentication.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  ENV['BASIC_AUTH_USER'] = 'admin'
+  ENV['BASIC_AUTH_PASSWORD'] = '8888'
+  
 end
