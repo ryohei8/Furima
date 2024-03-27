550cf1937e54ec2aee2dac42a2773f181322b52f
Ryohei Hachisuga <ryohei.8suga@gmail.com>
2024-03-27 16:58:30 +0900

    Prepare data by ActiveHash

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase_record
+
+  validates :category_id, :condition_id, :shipping_cost_burden_id, :province_id, :shipping_day_id,
+            numericality: { other_than: 1, message: "can't be blank" }
 end
