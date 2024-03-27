756c203532bc3a72b474686e88465416ff2b3165
Ryohei Hachisuga <ryohei.8suga@gmail.com>
2024-03-27 17:45:20 +0900

    Implemented ActiveStorage

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,11 @@ class ItemsController < ApplicationController
 
   def create
   end
+
+  private
+
+  def item_paramas
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_cost_burden_id, :province_id,
+                                 :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase_record
+  has_one_attached :image
 
   validates :category_id, :condition_id, :shipping_cost_burden_id, :province_id, :shipping_day_id,
             numericality: { other_than: 1, message: "can't be blank" }
