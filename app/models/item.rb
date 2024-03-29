e3901ce04611a98c87a84b411b3c3aa32a9c06e2
Ryohei Hachisuga <ryohei.8suga@gmail.com>
2024-03-29 15:29:44 +0900

    Remove duplicated validation.

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,8 +5,7 @@ class Item < ApplicationRecord
 
   validates :image, :name, :description, presence: true
 
-  validates :price, format: { with: /\A[0-9]+\z/ },
-                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   validates :category_id, :condition_id, :shipping_cost_burden_id, :province_id, :shipping_day_id,
             numericality: { other_than: 1, message: "can't be blank" }
